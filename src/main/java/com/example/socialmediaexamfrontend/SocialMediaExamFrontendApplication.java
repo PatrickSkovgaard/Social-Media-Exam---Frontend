358322013b35a473c465d0bf00aca26c77e99e92
Patrick <skovskiver@gmail.com>
2022-02-03 21:52:22 +0100

    First commit on frontend, getting ready to set up html files

---
+++ src/main/java/com/example/socialmediaexamfrontend/SocialMediaExamFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmediaexamfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaExamFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaExamFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialmediaexamfrontend/SocialMediaExamFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmediaexamfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaExamFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
